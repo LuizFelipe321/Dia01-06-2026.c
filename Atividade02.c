2e4cde0bc05831c5448e84e9999a855a3b0b6fcf
Luiz Felipe <102871467+LuizFelipe321@users.noreply.github.com>
2026-06-01 22:18:03 -0300

    Create Atividade02.c

---
+++ Atividade02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define k 5;
+
+int main(){
+    
+    int v[k], k, i;
+
+    printf("Digite um valor:"); 
+    scanf("%d", &k );
+    
+    for(i=0; i < n; i++){ 
+        
+      
