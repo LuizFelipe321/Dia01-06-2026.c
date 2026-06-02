c148239ac63ea151604cdbc9c1da65b862d5df3f
Luiz Felipe <102871467+LuizFelipe321@users.noreply.github.com>
2026-06-01 22:23:55 -0300

    Update Atividade02.c

--- Atividade02.c
+++ Atividade02.c
@@ -4,10 +4,20 @@
 int main(){
     
     int v[k], k, i;
+    
 
     printf("Digite um valor:"); 
     scanf("%d", &k );
     
+    if (k > 0){
     for(i=0; i < n; i++){ 
         
+        printf("Digite:");
+        scanf("%d", &v[i]);
+    }
+    }
+    
+    else{
+        printf("Esse vetor nao existe");
+    }
       
